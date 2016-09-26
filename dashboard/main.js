90eccd455c0c2b74328b1d9617fa65bc092c1137
Simon Lu <lusimon95@gmail.com>
2016-09-26 03:31:46 -0500

    basic layout done

--- dashboard/main.js
+++ dashboard/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     var date = new Date(2016, 8, 29);
     var now = new Date();
     var diff = (date.getTime()/1000) - (now.getTime()/1000);
-
+    
 	clock = $('.clock').FlipClock(diff, {
         clockFace: 'DailyCounter',
         autoStart: false,
